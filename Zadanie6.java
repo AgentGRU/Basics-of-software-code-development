7da16a315bfe2a4e29830c0bc03d4d216c990a06
AgentGRU <67546644+AgentGRU@users.noreply.github.com>
2020-06-30 23:18:29 +0300

    Add files via upload

---
+++ Zadanie6.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Zadanie6 {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 2;
+        System.out.print(x <= 4 && x >=-4 && y >= -3 && y <= 0 || y <= 4 && y >= 0 && x <= 2 && x >= -2);
+    }
+}
